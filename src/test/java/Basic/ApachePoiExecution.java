4e60f737bec16aa8acbe21a328d3d3b54049f41f
Radhika426 <radhikably426@gmail.com>
2024-02-05 12:08:14 +0530

    Demo Maven Project

---
+++ src/test/java/Basic/ApachePoiExecution.java
@@ -0,0 +1,13 @@
+package Basic;
+
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.testng.annotations.Test;
+
+public class ApachePoiExecution {
+	
+	@Test
+	public void poi() {
+		//WorkbookFactory
+	}
+
+}
---
+++ src/test/java/Basic/LaunchBrowserUsingTestNG.java
@@ -0,0 +1,12 @@
+package Basic;
+
+import org.testng.annotations.Test;
+
+public class LaunchBrowserUsingTestNG {
+	
+	@Test
+	public void test() {
+		
+	}
+
+}
